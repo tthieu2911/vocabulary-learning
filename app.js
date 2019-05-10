f5e710a0845b9f9eee2aad4e99386d3826b05197
Dung <dungtdq@necsv-hcm.com>
2019-05-10 16:25:50 +0800

    commit source

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Your node js server is running');
 });
