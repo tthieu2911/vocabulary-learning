fe7511658b328bedb301adb2910e6e1b1a68b753
Dung <dungtdq@necsv-hcm.com>
2019-05-14 15:23:47 +0800

    commit source

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-var connect = require('connect');
+/* var connect = require('connect');
 var serveStatic = require('serve-static');
 
 connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 8080...');
+}); */
+
+// in sublime
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+
+var indexRouter = require("./index");
+
+app.get('/', indexRouter);
+
+app.listen(port, function () {
+    console.log(`Example app listening on port !`);
 });
