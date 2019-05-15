8583e9c3df0bd740f3616d5fe702fdc31fc59b56
Dung <dungtdq@necsv-hcm.com>
2019-05-15 16:37:25 +0800

    commit source

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 8080...');
 }); */
+
+
+/* const express = require('express');
+const app = new express();
+
+app.get('/', function(request, response){
+    response.sendfile('index.html');
+}); */
+
+
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname));
+var port = 8000; // you can use any port
+app.listen(port);
+console.log('server on ' + port);
