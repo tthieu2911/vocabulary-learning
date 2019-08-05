94969db036471f043cd205e85f07bc35f0a7af81
Hieu <hieutt@necsv-hcm.com>
2019-08-05 16:11:04 +0700

    fix file Mode

--- js/hiraModule.js
+++ js/hiraModule.js
@@ -1,167 +1,169 @@
-
 var hiraModule = (function() {
 
-	var hira = {};
-	var dictionary = [];
-	var currentWord = {};
-	var ctrlCount = 0;
-	var firstWord_div;
-	var secondWord_input;
-	var listGroup_list;
-	var hfirstLoad = true;
-	var HiraFile;
-	
-	// private
-	hira.initEvent = function(){
-		// first words div
-		firstWord_div = $("#hira_contain .first_contain");
+    var hira = {};
+    var dictionary = [];
+    var currentWord = {};
+    var ctrlCount = 0;
+    var firstWord_div;
+    var secondWord_input;
+    var listGroup_list;
+    var hfirstLoad = true;
+    var HiraFile;
 
-		// second words div
-		secondWord_input = $("#hira_contain #second_words");
+    // private
+    hira.initEvent = function() {
+        // first words div
+        firstWord_div = $("#hira_contain .first_contain");
 
-		// list learnt words 
-		listGroup_list = $("#hira_contain .list-group");
+        // second words div
+        secondWord_input = $("#hira_contain #second_words");
 
-		// load data
-		this.reload(HiraFile);
-	}
+        // list learnt words 
+        listGroup_list = $("#hira_contain .list-group");
 
-	hira.reload = function(HiraFile) {
-		if(hfirstLoad){
-			importData('file/japanese.txt');
-			hfirstLoad = false;
-		}
-		else{
-			var cutFilename = HiraFile.split("/");
-			var len = cutFilename.length;
-			importData(cutFilename[len-1]);
-		}
+        // load data
+        this.reload(HiraFile);
+    }
 
-		setTimeout( function() {  // show words
-			showWord();
-		}, 200);
-		$("#hira_contain #resultLabel").text("");
-		listGroup_list.empty();
-		firstWord_div.html("");
-		secondWord_input.val("");
-		secondWord_input.focus();
-	}
+    hira.reload = function(HiraFile) {
+        if (hfirstLoad) {
+            importData('file/japanese.txt');
+            hfirstLoad = false;
+        } else {
+            var cutFilename = HiraFile.split("/");
+            var len = cutFilename.length;
+            importData(cutFilename[len - 1]);
+        }
 
-	hira.onEnterWords = function($this, event) {
-		if (event.keyCode === 13) {
-			this.checkWords($this);
-	    } else if (event.keyCode === 17) {
-	    	ctrlCount++;
-	    	if (ctrlCount === 1) {
-	    		this.showWordResult();
-	    	} else if (ctrlCount === 2){
-				$("#hira_contain #resultLabel").text("");
-	    		showWord();
-	    	}
-		}
-	}
+        setTimeout(function() { // show words
+            showWord();
+        }, 200);
+        $("#hira_contain #resultLabel").text("");
+        listGroup_list.empty();
+        firstWord_div.html("");
+        secondWord_input.val("");
+        secondWord_input.focus();
+    }
 
-	// Validate word
-	hira.checkWords = function() {
-		var inputWord = secondWord_input.val();
-		if(inputWord.trim() !== "") {
-			var wordCheck = currentWord.vietnamese;
-			if(wordCheck.indexOf(inputWord) !== -1) 
-				this.showResult(1);
-			else
-				this.showResult(2);
-		}
-	}
+    hira.onEnterWords = function($this, event) {
+        if (event.keyCode === 13) {
+            this.checkWords($this);
+        } else if (event.keyCode === 17) {
+            ctrlCount++;
+            if (ctrlCount === 1) {
+                this.showWordResult();
+            } else if (ctrlCount === 2) {
+                $("#hira_contain #resultLabel").text("");
+                showWord();
+            }
+        }
+    }
 
-	// Show result
-	hira.showResult = function(result) {
-		switch(result) {
-			case 1 : $("#hira_contain #resultLabel").removeClass("text-danger").addClass("text-success").text("OK");
-					//remove word from dictionary
-					addToLearntWords();
-					// remove wors from list
-					removeWordFromDictionary();
-					// show word
-					showWord();
-					break;
+    // Validate word
+    hira.checkWords = function() {
+        var inputWord = secondWord_input.val();
+        if (inputWord.trim() !== "") {
+            var wordCheck = currentWord.vietnamese;
+            if (wordCheck.indexOf(inputWord) !== -1)
+                this.showResult(1);
+            else
+                this.showResult(2);
+        }
+    }
 
-			case 2 : $("#hira_contain #resultLabel").removeClass("text-success").addClass("text-danger").text("Failed");
-					break;
+    // Show result
+    hira.showResult = function(result) {
+        switch (result) {
+            case 1:
+                $("#hira_contain #resultLabel").removeClass("text-danger").addClass("text-success").text("OK");
+                //remove word from dictionary
+                addToLearntWords();
+                // remove wors from list
+                removeWordFromDictionary();
+                // show word
+                showWord();
+                break;
 
-			case 3 : $("#hira_contain #resultLabel").addClass("text-success").removeClass("text-danger").text("Finished");
-					firstWord_div.text("");
-					break;
-		}
-		secondWord_input.val("");
-		secondWord_input.focus();
-	}
+            case 2:
+                $("#hira_contain #resultLabel").removeClass("text-success").addClass("text-danger").text("Failed");
+                break;
 
-	// Show word result
-	hira.showWordResult = function() {
-		$("#hira_contain #resultLabel").addClass("text-danger").removeClass("text-success").text(currentWord.vietnamese_ori);
-		secondWord_input.val("");
-		secondWord_input.focus();
-	}
+            case 3:
+                $("#hira_contain #resultLabel").addClass("text-success").removeClass("text-danger").text("Finished");
+                firstWord_div.text("");
+                break;
+        }
+        secondWord_input.val("");
+        secondWord_input.focus();
+    }
 
-	// Add To Learnt Words
-	var addToLearntWords = function() {
-		var kanji = currentWord.kanji !== "" ? '(' + currentWord.kanji + ')' : "";
-		firstWord_div.text(currentWord.hira_kata + kanji);
-		var li = "<a class='list-group-item'>"+ currentWord.hira_kata + kanji + ':<span> '+ currentWord.vietnamese_ori +'</span>' +"</a>";
-		listGroup_list.prepend(li);
-	}
+    // Show word result
+    hira.showWordResult = function() {
+        $("#hira_contain #resultLabel").addClass("text-danger").removeClass("text-success").text(currentWord.vietnamese_ori);
+        secondWord_input.val("");
+        secondWord_input.focus();
+    }
 
-	// Import data from local file
-	var importData = function(fileName) {
-		$.get(fileName, function(data) {
-     	dictionary = [];
-        var lines = data.split("\n");
-        for(var i = 0 ; i < lines.length; i++) {
-        	var words = lines[i].split("/");
-        	// create object words
-        	var wordsObj = {
-        		"hira_kata": words[0],
-        		"kanji": words.length > 2 ? words[1] : "",
-        		"vietnamese_ori": words.length > 2 ? words[2] : words[1],
-        		"vietnamese": words.length > 2 ? splitWords(words[2]) : splitWords(words[1])
-        	}
-	        dictionary.push(wordsObj);
-        }});
-	}
+    // Add To Learnt Words
+    var addToLearntWords = function() {
+        var kanji = currentWord.kanji !== "" ? '(' + currentWord.kanji + ')' : "";
+        firstWord_div.text(currentWord.hira_kata + kanji);
+        var li = "<a class='list-group-item'>" + currentWord.hira_kata + kanji + ':<span> ' + currentWord.vietnamese_ori + '</span>' + "</a>";
+        listGroup_list.prepend(li);
+    }
 
-	var splitWords = function(words) {
-		var arr = words.split(",").map(function(item) {
-		  return item.trim();
-		});
-		return arr;
-	}
+    // Import data from local file
+    var importData = function(fileName) {
+        $.get(fileName, function(data) {
+            dictionary = [];
+            var lines = data.split("\n");
+            for (var i = 0; i < lines.length; i++) {
+                var words = lines[i].split("/");
+                // create object words
+                var wordsObj = {
+                    "hira_kata": words[0],
+                    "kanji": words.length > 2 ? words[1] : "",
+                    "vietnamese_ori": words.length > 2 ? words[2] : words[1],
+                    "vietnamese": words.length > 2 ? splitWords(words[2]) : splitWords(words[1])
+                }
+                dictionary.push(wordsObj);
+            }
+        });
+    }
 
-	// Remove Word From Dictionary
-	var removeWordFromDictionary = function(){
-		for (var i =0; i < dictionary.length; i++)
-	   		if (dictionary[i].hira_kata === currentWord.hira_kata) {
-	     		dictionary.splice(i,1);
-	      		break;
-	   }
-	}
+    var splitWords = function(words) {
+        var arr = words.split(",").map(function(item) {
+            return item.trim();
+        });
+        return arr;
+    }
 
-	// Get random words
-	var randomWords = function() {
-	    var item = dictionary[Math.floor(Math.random()*dictionary.length)];
-	    return item;
-	};
+    // Remove Word From Dictionary
+    var removeWordFromDictionary = function() {
+        for (var i = 0; i < dictionary.length; i++)
+            if (dictionary[i].hira_kata === currentWord.hira_kata) {
+                dictionary.splice(i, 1);
+                break;
+            }
+    }
 
-	// Show word to screen
-	function showWord() {
-		currentWord = randomWords();
-		if(currentWord === null || currentWord === undefined) {
-			showResult(3);
-			return;
-		} 
-		var kanji = currentWord.kanji !== "" ? '(' + currentWord.kanji + ')' : "";
-		firstWord_div.text(currentWord.hira_kata + kanji);
-		// reset Ctrl count
-		ctrlCount = 0;
-	}
-	window.modules.hira = hira;
+    // Get random words
+    var randomWords = function() {
+        var item = dictionary[Math.floor(Math.random() * dictionary.length)];
+        return item;
+    };
+
+    // Show word to screen
+    function showWord() {
+        currentWord = randomWords();
+        if (currentWord === null || currentWord === undefined) {
+            showResult(3);
+            return;
+        }
+        var kanji = currentWord.kanji !== "" ? '(' + currentWord.kanji + ')' : "";
+        firstWord_div.text(currentWord.hira_kata + kanji);
+        // reset Ctrl count
+        ctrlCount = 0;
+    }
+    window.modules.hira = hira;
 }());
